656ebf94dfb8d529f95a335f9c9c844843c3b6a8
kodkodorod <114239871+kodkodorod@users.noreply.github.com>
2026-01-21 05:18:01 +0700

    Add files via upload

--- app.js
+++ app.js
@@ -142,7 +142,7 @@ function nav(path) {
 	var cur = window.current_drive_order || 0;
 	html += `<nav class="navbar navbar-expand-lg${UI.fixed_header ?' fixed-top': ''} ${UI.header_style_class}">
     <div class="container-fluid">
-  <a class="navbar-brand" href="/">${UI.logo_image ? '<img border="0" alt="'+UI.company_name+'" src="'+UI.logo_link_name+'" height="'+UI.logo_height+'" width="'+UI.logo_width+'">' : UI.logo_link_name}</a>
+  <a class="navbar-brand" href="/">${uiConfig.logo_image ? '<img border="0" oncontextmenu="return false;" draggable="false" alt="'+uiConfig.company_name+'" src="'+uiConfig.logo_link_name+'" height="'+uiConfig.logo_height+'" width="'+uiConfig.logo_width+'">' : uiConfig.logo_link_name}</a>
   <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
     <span class="navbar-toggler-icon"></span>
   </button>
