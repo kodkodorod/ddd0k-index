5c43478aae078e802ec21d7f21a0763255b19a1e
kodkodorod <114239871+kodkodorod@users.noreply.github.com>
2026-01-31 11:32:21 +0700

    Add files via upload

--- app.js
+++ app.js
@@ -141,7 +141,15 @@ function nav(path) {
 	var cur = window.current_drive_order || 0;
 	html += `<nav class="navbar navbar-expand-lg${UI.fixed_header ?' fixed-top': ''} ${UI.header_style_class}">
     <div class="container-fluid">
-  <a class="navbar-brand" href="/">${UI.logo_image ? '<img border="0" alt="' + UI.company_name + '" src="data:image/png;base64,' + UI.logo_link_name + '" height="' + UI.logo_height + '" width="' + UI.logo_width + '">' : UI.logo_link_name}</a>
+  <a class="navbar-brand" href="/">
+    ${UI.logo_image ? '<img id="main_logo" border="0" alt="'+UI.company_name+'" src="" height="'+UI.logo_height+'" width="'+UI.logo_width+'">' : UI.logo_link_name}
+  </a>
+  <script>
+    if (document.getElementById('main_logo')) {
+      document.getElementById('main_logo').src = atob("aHR0cHM6Ly90ZWxlZ3JhLnBoL2ZpbGUvYjk4NjRmYWQ2MzNjODE0MGRmNTIxLnBuZw==");
+        
+    }
+  </script>
   <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
     <span class="navbar-toggler-icon"></span>
   </button>
